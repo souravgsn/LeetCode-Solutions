95302793d307bd6bc9e42a4166b26fb50587e258
souravgsn <73579577+souravgsn@users.noreply.github.com>
2022-08-16 18:01:17 +0530

    Time: 4 ms (14.16%), Space: 42.1 MB (75.49%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.java
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -1,18 +1,28 @@
 class Solution {
-    public int numJewelsInStones(String jewels, String stones) {
-        int count =0;
-        int j = jewels.length()-1;
-        while(j>=0){
-        for (int i = 0; i < stones.length(); i++) {
-            if(jewels.charAt(j) == stones.charAt(i)){
-                count++;
-            }
-            else if(jewels.charAt(j) == stones.charAt(i)){
-                count++;
-            }
-        }
-        j--;
-    }
-        return count;
+   // public int numJewelsInStones(String jewels, String stones) {
+    //     int count =0;
+    //     int j = jewels.length()-1;
+    //     while(j>=0){
+    //     for (int i = 0; i < stones.length(); i++) {
+    //         if(jewels.charAt(j) == stones.charAt(i)){
+    //             count++;
+    //         }
+    //         else if(jewels.charAt(j) == stones.charAt(i)){
+    //             count++;
+    //         }
+    //     }
+    //     j--;
+    // }
+    //     return count;
+    //
+    //}
+     public int numJewelsInStones(String J, String S) {
+        int res = 0;
+        Set setJ = new HashSet();
+        for (char j: J.toCharArray())
+            setJ.add(j);
+        for (char s: S.toCharArray())
+            if (setJ.contains(s)) res++;
+        return res;
     }
 }
