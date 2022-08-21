29819891360022a0fc7351faad3177dfdc315e56
souravgsn <73579577+souravgsn@users.noreply.github.com>
2022-08-21 14:56:40 +0530

    Time: 11 ms (20.17%), Space: 41.9 MB (81.48%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        s = s.toLowerCase();
+        String str = s.substring(0,s.length()/2);
+        String str2 = s.substring(s.length()/2);
+        int count =0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i'
+              || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
+                count++;
+            }
+
+            if(str2.charAt(i) == 'a' || str2.charAt(i) == 'e' || str2.charAt(i) == 'i'
+              || str2.charAt(i) == 'o' || str2.charAt(i) == 'u'){
+                count--;
+            }
+        }
+        return (count ==0);
+    }
+}
