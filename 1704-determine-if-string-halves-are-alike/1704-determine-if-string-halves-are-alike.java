06f170b24e958b6282725119bcc2aa71402e71f6
souravgsn <73579577+souravgsn@users.noreply.github.com>
2022-08-21 14:59:25 +0530

    Time: 7 ms (45.39%), Space: 42.2 MB (72.26%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,20 +1,31 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        s = s.toLowerCase();
-        String str = s.substring(0,s.length()/2);
-        String str2 = s.substring(s.length()/2);
-        int count =0;
-        for(int i = 0; i < str.length(); i++){
-            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i'
-              || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
-                count++;
-            }
+//         s = s.toLowerCase();
+//         String str = s.substring(0,s.length()/2);
+//         String str2 = s.substring(s.length()/2);
+//         int count =0;
+//         for(int i = 0; i < str.length(); i++){
+//             if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i'
+//               || str.charAt(i) == 'o' || str.charAt(i) == 'u'){
+//                 count++;
+//             }
 
-            if(str2.charAt(i) == 'a' || str2.charAt(i) == 'e' || str2.charAt(i) == 'i'
-              || str2.charAt(i) == 'o' || str2.charAt(i) == 'u'){
-                count--;
-            }
+//             if(str2.charAt(i) == 'a' || str2.charAt(i) == 'e' || str2.charAt(i) == 'i'
+//               || str2.charAt(i) == 'o' || str2.charAt(i) == 'u'){
+//                 count--;
+//             }
+//         }
+//         return (count ==0);
+            HashSet<Character> set = new HashSet<>();
+        for (char c : "aeiouAEIOU".toCharArray())
+            set.add(c);
+    
+        int count = 0;
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (set.contains(s.charAt(i))) count++;
+            if (set.contains(s.charAt(i + s.length() / 2))) count--;
         }
-        return (count ==0);
+    
+        return count == 0;
     }
 }
