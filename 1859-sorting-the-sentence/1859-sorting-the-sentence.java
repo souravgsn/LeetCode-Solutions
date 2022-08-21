0e3f7759305a7aa19a689f8bc59394c750968d5e
souravgsn <73579577+souravgsn@users.noreply.github.com>
2022-08-21 14:40:01 +0530

    Time: 1 ms (92.18%), Space: 41.4 MB (84.13%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,16 +1,18 @@
 class Solution {
     public String sortSentence(String s) {
-      String[] str = s.split(" ");               
-	String[] res = new String[str.length];     
-	StringBuilder sb = new StringBuilder();     
-	int i = 0;                          
-	for (String elem : str) {
-		i = (int) (elem.charAt(elem.length() - 1) - '0');  
-		res[i - 1] = elem.substring(0, elem.length() - 1); 
-    }
-	for (i = 0; i < res.length - 1; i++)
-		sb.append(res[i]).append(" ");
-	sb.append(res[i]);
+       String[] na = s.split(" ");
+        String[] ans = new String[na.length];
+        StringBuilder sb = new StringBuilder();
+        int i =0;
+        for (String each : na) {
+              i = (int)(each.charAt(each.length()-1)-'0');
+              ans[i-1] = each.substring(0, each.length()-1);
+        }
+
+        for (i = 0; i < ans.length-1; i++) 
+          sb.append(ans[i]).append(" ");
+        
+        sb.append(ans[i]);
 	return sb.toString();
         
     }
