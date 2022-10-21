0278d45f58e5c2d9dbf5d6edb518ddec112f0d2c
souravgsn <73579577+souravgsn@users.noreply.github.com>
2022-10-21 20:13:31 +0530

    Time: 64 ms (19.67%), Space: 94.1 MB (36.98%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,19 +1,35 @@
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         if(k==0){
+//             return false;
+//         }
+//         for(int i = 0; i < nums.length-1 ; i++){
+//             int count = 0;
+            
+//             for(int j=i+1 ; j < nums.length ; j++){
+//                   count++;
+//                 if(nums[i] == nums[j]) {
+//                     return true;
+//                 }
+              
+//                 if(count>=k) break;
+//             }
+//         }
+//         return false;
+//     }
+    
+// }
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        if(k==0){
-            return false;
-        }
-        for(int i = 0; i < nums.length-1 ; i++){
-            int count = 0;
-            
-            for(int j=i+1 ; j < nums.length ; j++){
-                  count++;
-                if(nums[i] == nums[j]) {
-                    return true;
-                }
-              
-                if(count>=k) break;
-            }
+        if (k == 0) return false;
+
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int integer = nums[i];
+            if (hashMap.containsKey(integer) && i - hashMap.get(integer) <= k)
+                return true;
+            hashMap.put(integer, i);
         }
         return false;
     }
