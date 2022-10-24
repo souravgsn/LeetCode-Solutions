5f790925d2404a4d8442540ac4e87c13c992495f
souravgsn <73579577+souravgsn@users.noreply.github.com>
2022-10-24 22:14:13 +0530

    Time: 1229 ms (18.34%), Space: 0B (100.00%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,8 @@
+SELECT    D.name AS DEPARTMENT , 
+          E.name AS EMPLOYEE , 
+		  E.salary
+     FROM EMPLOYEE AS E 
+	               JOIN 
+				   DEPARTMENT AS D 
+				   ON E.departmentId = D.id
+     WHERE E.salary =  ( SELECT MAX(salary) FROM EMPLOYEE WHERE departmentID = E.departmentID);
