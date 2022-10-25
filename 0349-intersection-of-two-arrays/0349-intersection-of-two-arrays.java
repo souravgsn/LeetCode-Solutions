e463827963331c47148b4e512c5e877619fd5929
souravgsn <73579577+souravgsn@users.noreply.github.com>
2022-10-25 22:59:10 +0530

    Time: 5 ms (71.57%), Space: 44.8 MB (6.58%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,25 +1,25 @@
 class Solution {
-     public static int[] intersection(int[] nums1, int[] nums2) {
+    public static int[] intersection(int[] nums1, int[] nums2) {
         HashSet<Integer> set1 = new HashSet<>();
-        List<Integer> ans = new ArrayList<>();
+        HashSet<Integer> set2 = new HashSet<>();
+ 
         for(int i=0 ; i<nums1.length;i++){
             set1.add(nums1[i]);
         }
         for (int i = 0; i < nums2.length; i++) {
-            int n = nums2[i];
-            if(set1.contains(n)){
-                if(!ans.contains(n)){
-                    ans.add(n);
-                }
-            }
+            set2.add(nums2[i]);
         }
-        
-        return toIntArray(ans);
+        set1.retainAll(set2);
+        return toIntArray(set1);
     }
-    public static int[] toIntArray(List<Integer> list) {
+
+    public static int[] toIntArray(HashSet<Integer> list) {
         int[] ret = new int[list.size()];
-        for(int i = 0; i < ret.length; i++)
-          ret[i] = list.get(i);
+        Iterator<Integer> it = list.iterator();  
+        int i=0;
+        while (it.hasNext()) {  
+            ret[i++] = it.next();
+        }  
         return ret;
     }
 }
