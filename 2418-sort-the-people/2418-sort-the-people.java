37711e3e85afb49ba3b92bb4b595779fcf6cb985
souravgsn <73579577+souravgsn@users.noreply.github.com>
2022-10-27 11:25:14 +0530

    Time: 19 ms (46.01%), Space: 55.4 MB (7.45%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.java
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -1,16 +1,17 @@
-    class Solution {
-        public String[] sortPeople(String[] names, int[] heights) {
-            String[] ans = new String[names.length];
-            for (int i = 0; i < heights.length; i++) {
-                int max = i;
-                for (int j = 0; j < heights.length; j++) {
-                    if(heights[j] > heights[max]){
-                        max = j;
-                    }
-                }
-                heights[max] =0;
-                ans[i] = names[max];
-            }
-            return ans;
-        }
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+       Map<Integer, String> map = new HashMap<>();
+       for(int i = 0; i < names.length; i ++) {
+           map.put(heights[i], names[i]);
+       }
+       Arrays.sort(heights);
+
+       String[] results = new String[heights.length];
+       int index = 0;
+       for(int i = names.length - 1; i >= 0; i --) {
+           results[index] = map.get(heights[i]);
+           index ++;
+       } 
+       return results;
     }
+}
