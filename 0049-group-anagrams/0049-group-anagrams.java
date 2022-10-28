08875564a402fdadac520866a55794001f785ebd
souravgsn <73579577+souravgsn@users.noreply.github.com>
2022-10-28 20:26:07 +0530

    Time: 7 ms (96.04%), Space: 45.5 MB (90.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,51 +1,14 @@
 class Solution {
-    public static String sortString(String inputString)
-    {
-        // Converting input string to character array
-        char tempArray[] = inputString.toCharArray();
- 
-        // Sorting temp array using
-        Arrays.sort(tempArray);
- 
-        // Returning new sorted string
-        return new String(tempArray);
-    }
-    public static List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> ans = new ArrayList<>();
-        if (strs == null || strs.length == 0) {
-            return ans;
-        }   
-        
-        if (strs.length == 1) {
-            ans.add(Arrays.asList(strs));
-            return ans;
-        }
-        String[] cpy = strs.clone();
-        for (int i = 0; i < cpy.length; i++) {
-            cpy[i] = sortString(cpy[i]);            
-        }
-        
-        for (int i = 0; i < cpy.length; i++) {
-            List<String> ls = new ArrayList<>();
-            String x = cpy[i];
-            if(x != null){
-                ls.add(strs[i]);
+    public List<List<String>> groupAnagrams(String[] strs) {
+            if (strs == null || strs.length == 0) return new ArrayList<>();
+            Map<String, List<String>> map = new HashMap<>();
+            for (String s : strs) {
+                char[] ca = s.toCharArray();
+                Arrays.sort(ca);
+                String keyStr = String.valueOf(ca);
+                if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+                map.get(keyStr).add(s);
             }
-            if(cpy[i] != null){
-            for (int j = i+1; j < cpy.length; j++) {
-              
-                if(cpy[i].equals(cpy[j]) && cpy[j] != null){
-                
-                    ls.add(strs[j]);
-                    cpy[j] = null;
-                }
-
-            }
-        
-            ans.add(ls);
+            return new ArrayList<>(map.values());
         }
-        }
-        return ans;
-    }
- 
 }
