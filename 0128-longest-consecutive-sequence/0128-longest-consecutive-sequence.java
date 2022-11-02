b7ccea1ee8fdfc20b7c8360a4c87b4d3ffde0757
souravgsn <73579577+souravgsn@users.noreply.github.com>
2022-11-02 17:37:54 +0530

    Time: 12 ms (99.75%), Space: 52.3 MB (97.16%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,31 +1,25 @@
-import java.util.Arrays;
-
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        if(nums.length == 0 ) return 0;
-        if(nums.length == 1 ) return 1;
+    public static int longestConsecutive(int[] nums) {
+        if(nums.length == 0 || nums == null){
+            return 0;
+        }
+        
         Arrays.sort(nums);
-        int ans =0;
-        int count =0;
-        int i=0;    
-        while(i< nums.length-1){
-            if(nums[i] == nums[i+1]-1 ){
-                count++;
-                ans = Math.max(ans,count);
-            }else if(nums[i] == nums[i+1]){
-                i++;
-                continue;
-                
+        
+        int ans = 1;
+        int prev = nums[0];
+        int cur = 1;
+        
+        for(int i = 1;i < nums.length;i++){
+            if(nums[i] == prev+1){
+                cur++;
             }
-            else{
-                count = 0;
+            else if(nums[i] != prev){
+                cur = 1;
             }
-            i++;
+            prev = nums[i];
+            ans = Math.max(ans, cur);
         }
-        int n = nums.length -1;
-        if(nums[n] == nums[n-1]+1 || nums[n] == nums[n-1] && count == ans){
-            return ans +1;
-        }
-        return ans+1;
+        return ans;
     }
 }
