7a401367da588000777a96336b8477c03b551f67
souravgsn <73579577+souravgsn@users.noreply.github.com>
2022-11-17 06:04:04 +0530

    Time: 4 ms (76.60%), Space: 43.9 MB (35.19%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String destCity(List<List<String>> paths) {
+		HashSet<String> set = new HashSet<>();
+		for (List<String> list : paths) {
+			set.add(list.get(0));
+        }
+		for (List<String> list : paths) {
+			if(!set.contains(list.get(1))) return list.get(1);
+        }
+		return null;
+
+    }
+}
