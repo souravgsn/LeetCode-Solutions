f4af0ad2a1b5563460d9a6b06d0afdd9dd2fd7e5
souravgsn <73579577+souravgsn@users.noreply.github.com>
2022-11-29 00:43:46 +0530

    Time: 1 ms (85.07%), Space: 41.2 MB (61.33%) - LeetHub

--- 1556-thousand-separator/1556-thousand-separator.java
+++ 1556-thousand-separator/1556-thousand-separator.java
@@ -1,24 +1,24 @@
 class Solution {
-public String thousandSeparator(int n) {
+    public String thousandSeparator(int n) {
+        if(n==0) return "0";
+        if(n<1000) return n+"";
 
-    if(n<1000) return n+"";
-    
-    String str=""+n;
-    StringBuilder res=new StringBuilder();
-    
-    int count=0;
-    for(int i=str.length()-1;i>=0;i--){
-        
-        res.append(str.charAt(i));
-        count++;
-        // oonly append '.' after count became 3.
-        if(count ==3 && i!=0){
-            res.append('.');
-            count=0;
+        String str=""+n;
+        StringBuilder res=new StringBuilder();
+
+        int count=0;
+        for(int i=str.length()-1;i>=0;i--){
+
+            res.append(str.charAt(i));
+            count++;
+            // oonly append '.' after count became 3.
+            if(count ==3 && i!=0){
+                res.append('.');
+                count=0;
+            }
+
         }
-        
+
+        return res.reverse().toString();
     }
-    
-    return res.reverse().toString();
-}
 }
