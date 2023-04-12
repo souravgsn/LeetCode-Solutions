54a97500577f35460bf452a60a4361c037dfa116
souravgsn <73579577+souravgsn@users.noreply.github.com>
2023-04-12 22:16:09 +0530

    Added solution - LeetHub

--- Dominant Pairs - GFG/dominant-pairs.java
+++ Dominant Pairs - GFG/dominant-pairs.java
@@ -59,13 +59,14 @@ class Solution {
     public static int dominantPairs(int n, int[] arr) {
         Arrays.sort(arr,0,n/2);
         Arrays.sort(arr,n/2,n);
-        for(int j=n/2;j<n;j++) arr[j]=arr[j]*5;
+        
+        // for(int j=n/2;j<n;j++) arr[j]=arr[j]*5;
         
         int i=0 ,j= n/2;
         int ans=0;
         
         while(i< n/2 && j<n ){
-            if(arr[i]>= arr[j]){
+            if(arr[i]>= 5 * arr[j]){
                 ans+=n/2-i;
                 j++;
             }
